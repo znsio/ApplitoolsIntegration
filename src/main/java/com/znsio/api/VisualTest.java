5e5636244f76e1a0034a2d640a85f542202101b4
grover-ankit11 <grover_ankit11@yahoo.in>
2023-09-21 01:00:41 +0530

    Added implementation for App tests execution

--- src/main/java/com/znsio/api/VisualTest.java
+++ src/main/java/com/znsio/api/VisualTest.java
@@ -1,9 +1,14 @@
 package com.znsio.api;
 
-import com.applitools.eyes.*;
+import com.applitools.eyes.BatchInfo;
+import com.applitools.eyes.EyesRunner;
+import com.applitools.eyes.StdoutLogHandler;
+import com.applitools.eyes.TestResults;
+import com.applitools.eyes.TestResultsStatus;
+import com.applitools.eyes.TestResultsSummary;
+import com.applitools.eyes.TestResultContainer;
 import com.applitools.eyes.selenium.ClassicRunner;
 import com.applitools.eyes.selenium.Configuration;
-import com.applitools.eyes.selenium.Eyes;
 import com.applitools.eyes.visualgrid.services.RunnerOptions;
 import com.applitools.eyes.visualgrid.services.VisualGridRunner;
 import com.znsio.api.utils.Config;
@@ -21,15 +26,16 @@ import java.util.Arrays;
 import static com.znsio.api.ApplitoolsConfigurationManager.config;
 
 public class VisualTest {
-    private static WebDriver driver;
-    protected Eyes eyes;
+    private static WebDriver webDriver;
+    protected com.applitools.eyes.selenium.Eyes eyesOnWeb;
+    protected com.applitools.eyes.appium.Eyes eyesOnApp;
     private static BatchInfo batch;
     private static Configuration eyesConfig;
     private static EyesRunner runner;
     private static final Logger LOGGER = Logger.getLogger(VisualTest.class.getName());
 
-    public static void driverSetupForVisualTest(WebDriver webDriver) {
-        driver = webDriver;
+    public static void driverSetupForVisualTest(WebDriver wDriver) {
+        webDriver = wDriver;
     }
 
     @BeforeSuite
@@ -43,41 +49,35 @@ public class VisualTest {
         batch = new BatchInfo(eyesConfig.getAppName() + "-" + suite.getSuite().getName() + "-" + suite.getName());
         ApplitoolsConfigurationManager.setBatchProperties(batch);
         eyesConfig.setBatch(batch);
-        if (ApplitoolsConfigurationManager.isUltraFastGridEnabled()) {
+        if (ApplitoolsConfigurationManager.isPlatformWeb() && ApplitoolsConfigurationManager.isUltraFastGridEnabled()) {
             ApplitoolsConfigurationManager.setUFGBrowserConfig(eyesConfig);
+            runner = new VisualGridRunner(new RunnerOptions()
+                    .testConcurrency(ApplitoolsConfigurationManager.getConcurrency()));
+        } else {
+            runner = new ClassicRunner();
         }
-        runner = ApplitoolsConfigurationManager.isUltraFastGridEnabled() ? new VisualGridRunner(new RunnerOptions()
-                .testConcurrency(ApplitoolsConfigurationManager.getConcurrency())) : new ClassicRunner();
         runner.setDontCloseBatches(true);
     }
 
     @BeforeMethod
     public void initiateVisualTests(Method method) {
         LOGGER.info("@BeforeMethod of VisualTest called: " + method.getName());
-        eyes = new Eyes(runner);
-        if (ApplitoolsConfigurationManager.isLogsEnabled()) {
-            eyes.setLogHandler(new StdoutLogHandler(ApplitoolsConfigurationManager.isLogsEnabled()));
+        if (ApplitoolsConfigurationManager.isPlatformWeb()) {
+            initiateVisualWebTests(method);
+        } else {
+            initiateVisualAppTests(method);
         }
-        eyes.setConfiguration(eyesConfig);
-        boolean isTestPartOfVisualGroup = Arrays.toString(method.getAnnotation(Test.class).groups())
-                .contains(ApplitoolsConfigurationManager.getVisualValidationGroupName());
-        LOGGER.info("Is test part of '" +
-                ApplitoolsConfigurationManager.getVisualValidationGroupName() + "' group: " + isTestPartOfVisualGroup);
-        eyes.setIsDisabled((!isTestPartOfVisualGroup || ApplitoolsConfigurationManager.isApplitoolsDisabled()));
-        LOGGER.info("Is Applitools enabled for " + method.getName() + ": " + !eyes.getIsDisabled());
-        eyes.open(driver,
-                eyesConfig.getAppName() + "-" +
-                        config.getProperty(Config.PLATFORM).toUpperCase() + "-" +
-                        eyesConfig.getEnvironmentName(),
-                method.getDeclaringClass().getSimpleName() + "-" + method.getName(),
-                eyesConfig.getViewportSize());
     }
 
     @AfterMethod
     public void closeVisualTest(ITestResult iTestResult, ITestContext context) {
         LOGGER.info("@AfterMethod of VisualTest called: Waiting for visual validation results of test: " +
                 iTestResult.getName());
-        eyes.closeAsync();
+        if (ApplitoolsConfigurationManager.isPlatformWeb()) {
+            eyesOnWeb.closeAsync();
+        } else {
+            eyesOnApp.closeAsync();
+        }
         TestResultsSummary allTestResults = runner.getAllTestResults(false);
         TestResultContainer[] results = allTestResults.getAllResults();
         LOGGER.info(String.format("Number of cross-browser tests run for test: %s: %d%n",
@@ -100,6 +100,47 @@ public class VisualTest {
     public void closeBatch() {
         LOGGER.info("@AfterSuite of VisualTest called: Close Visual Test batch");
         batch.setCompleted(true);
+    }
+
+    private void initiateVisualWebTests(Method method) {
+        eyesOnWeb = new com.applitools.eyes.selenium.Eyes(runner);
+        if (ApplitoolsConfigurationManager.isLogsEnabled()) {
+            eyesOnWeb.setLogHandler(new StdoutLogHandler(ApplitoolsConfigurationManager.isLogsEnabled()));
+        }
+        eyesOnWeb.setConfiguration(eyesConfig);
+        boolean isTestPartOfVisualGroup = Arrays.toString(method.getAnnotation(Test.class).groups())
+                .contains(ApplitoolsConfigurationManager.getVisualValidationGroupName());
+        LOGGER.info("Is test part of '" +
+                ApplitoolsConfigurationManager.getVisualValidationGroupName() + "' group: " + isTestPartOfVisualGroup);
+        eyesOnWeb.setIsDisabled((!isTestPartOfVisualGroup || ApplitoolsConfigurationManager.isApplitoolsDisabled()));
+        LOGGER.info("Is Applitools enabled for " + method.getName() + ": " + !eyesOnWeb.getIsDisabled());
+        eyesOnWeb.open(webDriver,
+                eyesConfig.getAppName() + "-" +
+                        config.getProperty(Config.PLATFORM).toUpperCase() + "-" +
+                        eyesConfig.getEnvironmentName(),
+                method.getDeclaringClass().getSimpleName() + "-" + method.getName(),
+                eyesConfig.getViewportSize());
+    }
+
+    private void initiateVisualAppTests(Method method) {
+        eyesOnApp = new com.applitools.eyes.appium.Eyes(runner);
+        if (ApplitoolsConfigurationManager.isLogsEnabled()) {
+            eyesOnApp.setLogHandler(new StdoutLogHandler(ApplitoolsConfigurationManager.isLogsEnabled()));
+        }
+        eyesOnApp.setConfiguration(eyesConfig);
+        boolean isTestPartOfVisualGroup = Arrays.toString(method.getAnnotation(Test.class).groups())
+                .contains(ApplitoolsConfigurationManager.getVisualValidationGroupName());
+        LOGGER.info("Is test part of '" +
+                ApplitoolsConfigurationManager.getVisualValidationGroupName() + "' group: " + isTestPartOfVisualGroup);
+        eyesOnApp.setIsDisabled((!isTestPartOfVisualGroup || ApplitoolsConfigurationManager.isApplitoolsDisabled()));
+        LOGGER.info("Is Applitools enabled for " + method.getName() + ": " + !eyesOnApp.getIsDisabled());
+        //TODO: The driver below will be changed to Appium driver instance once
+        // App tests implementation is completed in getting-started-with-rp-ap repo
+        eyesOnApp.open(webDriver,
+                eyesConfig.getAppName() + "-" +
+                        config.getProperty(Config.PLATFORM).toUpperCase() + "-" +
+                        eyesConfig.getEnvironmentName(),
+                method.getDeclaringClass().getSimpleName() + "-" + method.getName());
     }
 
     private boolean handleTestResults(TestResults result) {
